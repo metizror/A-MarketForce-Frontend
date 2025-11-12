d9adb2cf08381e7435b6072bb6c79f61f4c63677
Umesh <umesh.dangar@metizsoft.com>
2025-11-12 17:42:16 +0530

    added setting apis

--- src/app/api/auth/me/route.ts
+++ src/app/api/auth/me/route.ts
@@ -32,11 +32,13 @@ export async function GET(request: NextRequest) {
 export async function POST(request: NextRequest) {
   try {
     const { error, admin } = await requireAdminAuth(request);
+    console.log("admin", admin);
     if (error) {
       return error;
     }
 
     const { data } = await request.json();
+    console.log("data", data);
     if (!data) {
       return NextResponse.json(
         { message: "Data is required" },
@@ -45,7 +47,7 @@ export async function POST(request: NextRequest) {
     }
 
     await connectToDatabase();
-    const adminAuth = await AdminAuth.findById(admin?._id).select("-password");
+    const adminAuth = await AdminAuth.findById(admin?._id);
     if (!adminAuth) {
       return NextResponse.json(
         { message: "Admin auth not found" },
@@ -53,21 +55,22 @@ export async function POST(request: NextRequest) {
       );
     }
 
-    if (data.password !== adminAuth.password) {
+    if (!adminAuth.password) {
+      return NextResponse.json(
+        { message: "Password not found for this account" },
+        { status: 400 }
+      );
+    }
+
+    let currentPassword = await bcrypt.compare(data.password, adminAuth.password);
+    if (!currentPassword) {
       return NextResponse.json(
         { message: "Current password is incorrect" },
         { status: 400 }
       );
     }
 
-    if (data.password !== data.confirmPassword) {
-      return NextResponse.json(
-        { message: "Passwords do not match" },
-        { status: 400 }
-      );
-    }
-
-    const hashedPassword = await bcrypt.hash(data.password, 10);
+    const hashedPassword = await bcrypt.hash(data.newPassword, 10);
     await AdminAuth.findByIdAndUpdate(
       admin?._id,
       { ...data, password: hashedPassword },
@@ -78,6 +81,7 @@ export async function POST(request: NextRequest) {
       { status: 200 }
     );
   } catch (error) {
+    console.log("error", error);
     return NextResponse.json(
       { message: "Internal server error" },
       { status: 500 }
